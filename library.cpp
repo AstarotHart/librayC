2e8a7805230171cf982c34185098e41aab982c4d
AstarotH <alejo_pin3da@hotmail.com>
2015-05-30 02:57:27 -0500

    Libros, monografias y articulo.

--- library.cpp
+++ library.cpp
@@ -4,7 +4,7 @@
 #include <ctype.h>
 
 
-/* ESTRUCTURAS */
+/* ----------------------- ESTRUCTURAS ----------------------- */
 
 // Estructura LIBRO
 typedef struct libro
@@ -14,7 +14,7 @@ typedef struct libro
 	char 	autors[60];
 	char 	tema[120];
 	int		anio_publicacion;
-}
+};
 
 // Estructura MONOGRAFIA
 typedef struct monografia
@@ -22,7 +22,7 @@ typedef struct monografia
 	char 	titulo[120];
 	char 	autors[60];
 	char 	tema[120];
-}
+};
 
 // Estructura ARTICULO
 typedef struct articulo
@@ -32,7 +32,7 @@ typedef struct articulo
 	char 	tema[120];
 	char	nombre_revista[120];
 	char	paginas[120];
-}
+};
 
 // Estructura AUDIO
 typedef struct audio
@@ -42,7 +42,7 @@ typedef struct audio
 	char 	tema[120];
 	char	formato[20];
 	char	duracion[60];
-}
+};
 
 // Estructura VIDEO
 typedef struct video
@@ -52,7 +52,7 @@ typedef struct video
 	char 	tema[120];
 	char	formato[20];
 	char	duracion[60];
-}
+};
 
 // Estructura RECURSO WEB
 typedef struct uri
@@ -62,13 +62,20 @@ typedef struct uri
 	char 	tema[120];
 	char	uri[300];
 	char	idioma[120];
-}
+};
 
 /* PROTOTIPOS */
 
 
+/* VARIABLES GLOBALES */
+libro libro1;
+monografia monografia1;
+articulo articulo1;
 
-/* FUNCIONES */
+FILE* archivo= NULL;
+char* nombrearchivo = "Library.txt";
+
+/* ========================= FUNCIONES ======================= */
 
 
 /* -------------------------- MENUS -------------------------- */
@@ -76,22 +83,22 @@ typedef struct uri
 // Menu Principal
 void opciones (void)
 {
-     printf ("		    MENU PRINCIPAL         \n\n\n");
+     printf ("		    MENU PRINCIPAL         	\n\n\n");
      printf (" 1.  Buscar recurso.					\n");
-     printf (" 2.  Administrar Recursos.				\n");
-     printf (" 3.  Configuracion Biblioteca.			\n");
-     printf (" 0.  Salir   \n\n");
+     printf (" 2.  Administrar Recursos.			\n");
+     printf (" 3.  Configuracion Biblioteca.		\n");
+     printf (" 0.  Salir   						  \n\n");
 }
 
 
 // Menu Busqueda
 void menu_busqueda (void)
 {
-     printf ("		       BUSQUEDA  		   \n\n\n");
+     printf ("		       BUSQUEDA  		   	\n\n\n");
      printf (" 1.  Buscar por Autor.				\n");
      printf (" 2.  Buscar por Titulo.				\n");
-     printf (" 3.  Buscar por tema.				\n");
-     printf (" 0.  Volver   \n\n");
+     printf (" 3.  Buscar por tema.					\n");
+     printf (" 0.  Volver   					  \n\n");
 }
 
 // Menu Administrar Recursos
@@ -101,7 +108,17 @@ void menu_admon_recursos(void)
      printf (" 1.  Agregar recursos.				\n");
      printf (" 2.  Modificar datos recursos.		\n");
      printf (" 3.  Borrar un recurso.				\n");
-     printf (" 0.  Volver   \n\n");
+     printf (" 0.  Volver   					  \n\n");
+}
+
+// Menu Administrar Recursos Agregar
+void menu_admon_recursos_agregar(void)
+{
+     printf ("		   INGRESAR RECURSOS		\n\n\n");
+     printf (" 1.  Ingresar Libro.					\n");
+     printf (" 2.  Ingresar Monografia.				\n");
+     printf (" 3.  Ingresar Articulo.				\n");
+     printf (" 0.  Volver   					  \n\n");
 }
 
 // Menu Configuracion Biblioteca
@@ -110,7 +127,7 @@ void menu_config (void)
      printf ("		     CONFIGURACION  		\n\n\n");
      printf (" 1.  Administrar tipos.				\n");
      printf (" 2.  Administrar Metadatos.			\n");
-     printf (" 0.  Volver   \n\n");
+     printf (" 0.  Volver   					  \n\n");
 }
 
 // Menu Configuracion Biblioteca - Administrar tipos
@@ -120,7 +137,7 @@ void menu_config_tipos (void)
      printf (" 1.  Crear recurso.					\n");
      printf (" 2.  Modificar recurso.				\n");
      printf (" 3.  Borrar recurso.					\n");
-     printf (" 0.  Volver   \n\n");
+     printf (" 0.  Volver   					  \n\n");
 }
 
 // Menu Configuracion Biblioteca - Administrar Metadatos
@@ -130,23 +147,16 @@ void menu_config_metadatos (void)
      printf (" 1.  Crear metadatos.					\n");
      printf (" 2.  Modificar metadatos.				\n");
      printf (" 3.  Borrar metadatos.				\n");
-     printf (" 0.  Volver   \n\n");
+     printf (" 0.  Volver   					  \n\n");
 }
 
 
 /* ----------------------- FUNCIONES RECURSOS ----------------------- */
 
+// Ingresar LIBRO
 void crear_recurso_libro (void)
 {	
-	libro libro1;
-	
-	FILE* archivo= NULL;
-    char* nombrearchivo = "Library.txt";
-
-    //recibo los valores por entrada del usuario
-    /*Ojo: "w" me sobreescribe un archivo si ya existe*/
     archivo= fopen(nombrearchivo, "a"); //abro el archivo
-    //if (archivo == NULL)return-1;//si no pudo abrir lo cierro
 	
     //Ingresar datos
     printf("Ingrese ISBN: ");
@@ -170,7 +180,6 @@ void crear_recurso_libro (void)
     scanf("%d",&libro1.anio_publicacion);
     
     //imprimo los valores
-    printf("ISBN: %d\nTITULO: %s \nAUTOR: %s \nTEMA: %s \nA. PUBLIC: %d",libro1.ISBN, libro1.titulo, libro1.autors, libro1.tema, libro1.anio_publicacion);
     //printf("ISBN: %d\nTITULO: %s \nAUTOR: %s \nTEMA: %s \nA. PUBLIC: %d",libro1.ISBN, libro1.titulo, libro1.autors, libro1.tema, libro1.anio_publicacion);
     
     // Escribir datos en el archivo
@@ -184,13 +193,79 @@ void crear_recurso_libro (void)
     fclose(archivo);
 }
 
+// Ingresar MONOGRAFIA
+void crear_recurso_monografia (void)
+{	
+
+    archivo= fopen(nombrearchivo, "a"); //abro el archivo
+	
+    //Ingresar datos 
+    printf("\ningrese el Titulo: ");
+    fflush(stdin);
+    gets(monografia1.titulo);
+    
+    printf("\nIngrese el Autor(es): ");
+    fflush(stdin);
+    gets(monografia1.autors);
+    
+    printf("\nIngrese Tema: ");
+    fflush(stdin);
+    gets(monografia1.tema);
+
+    // Escribir datos en el archivo
+    fprintf(archivo,"monografia|");
+    fprintf(archivo,"%s|",monografia1.titulo);
+    fprintf(archivo,"%s|",monografia1.autors);
+    fprintf(archivo,"%s.\n",monografia1.tema);
+    
+    fclose(archivo);
+}
+
+// Ingresar ARTICULO
+void crear_recurso_articulo (void)
+{	
+
+    archivo= fopen(nombrearchivo, "a"); //abro el archivo
+	
+    //Ingresar datos 
+    printf("\ningrese el Titulo: ");
+    fflush(stdin);
+    gets(articulo1.titulo);
+    
+    printf("\nIngrese el Autor(es): ");
+    fflush(stdin);
+    gets(articulo1.autors);
+    
+    printf("\nIngrese Tema: ");
+    fflush(stdin);
+    gets(articulo1.tema);
+
+    printf("\nIngrese Nombre de la revista: ");
+    fflush(stdin);
+    gets(articulo1.nombre_revista);
+
+    printf("Ingrese rango de paginas articulo: ");
+    fflush(stdin);
+    gets(articulo1.paginas);
+
+    // Escribir datos en el archivo
+    fprintf(archivo,"monografia|");
+    fprintf(archivo,"%s|",articulo1.titulo);
+    fprintf(archivo,"%s|",articulo1.autors);
+    fprintf(archivo,"%s|",articulo1.tema);
+    fprintf(archivo,"%s|",articulo1.nombre_revista);
+    fprintf(archivo,"%s.\n",articulo1.paginas);
+    
+    fclose(archivo);
+}
+
 
 
 
 /* MAIN */
 int main (void)
 {
-	int opc=-1, opc_busq=-1, opc_admin=-1, opc_config=-1, opc_config_tipos=-1, opc_config_metadatos=-1;
+	int opc=-1, opc_busq=-1, opc_admin=-1, opc_config=-1, opc_config_tipos=-1, opc_config_metadatos=-1, opc_admin_agrear=-1;
 	
 	do {
         system ("cls"); 
@@ -218,7 +293,8 @@ int main (void)
 					break;
 		
 			case 2:
-				
+					
+					menu_admin:
 					system ("cls"); 
         
         			menu_admon_recursos();
@@ -226,11 +302,46 @@ int main (void)
         			scanf ("%d",&opc_admin);
 					
 					//Menu Administracion Recursos
-					/*switch(opc_admin) 
+					switch(opc_admin) 
 					{
 						case 1:
+								menu_admin_agregar:
+								system ("cls"); 
+        
+			        			menu_admon_recursos_agregar();
+								printf ("Ingrese la opción seleccionada:  "); 
+			        			scanf ("%d",&opc_admin_agrear);
+
+			        			switch(opc_admin_agrear)
+								{
+									case 1:
+											crear_recurso_libro ();
+											getch();
+											break;
+
+									case 2:
+											crear_recurso_monografia ();
+											getch();
+											break;
+
+									case 3:
+											crear_recurso_articulo ();
+											getch();
+											break;
+
+									/*case 4:
+											crear_recurso_libro ();
+											getch();
+											break;*/
+												
+									case 0: 
+											goto menu_admin_agregar;
+											break;
+								}
+			        						
+								break;
 						
-					}*/
+					}
 					
 					break;
 					
@@ -258,6 +369,7 @@ int main (void)
 								{
 									case 0: 
 											goto menu_config;
+											break;
 								}
         						
 								break;
